515f5576796328fe503ec7a0f433c796ab29aa0e
Kim Taeseon <kimtaeseon@Kimui-iMac-Pro-2.local>
2021-04-02 00:00:34 +0900

    chapter 2

--- pacman-x/lec02/my_app/lib/domain/auth/email_address.dart
+++ pacman-x/lec02/my_app/lib/domain/auth/email_address.dart
@@ -1,20 +1,29 @@
 class EmailAddress {
   final String value;
 
-  /* 1.1 const Constructor
-  const Constructor로 object를 만들게 되면 초기화 과정에서 const EmailAddress("sr9872@naver.com")  
-  와 같은 형태로 초기화 할 수 있다. 만약 이와 같이 초기화 하게 된다면 서로 다른 object를 만들어 performace에 문제를
-  일으키는 것이 아니라, 재사용가능하게 할 수 있다.
+/* 2.2 factory
+  factory는 constructor가 인스턴스를 만드는 것 이상의 것들을 수행하고자 할때 사용한다.
+  1. cache로부터 인스턴스를 반환하거나
+  2. 하위 클레스의 인스턴스를 반환하거나
+  3. 또는 initializer list에서 다루어지지 않는 로직을 사용하는 final 변수를 초기화 하거나
+  4. 물론 싱글톤에서도 사용한다.
 */
 
-  const EmailAddress(this.value);
+/* 2.4 factory Constructor
+validateEmailAddress에서도 반환값이 string 타입이기 때문에 아래와 같이 표현한다.
+*/
 
-  /* 1.2 == Operator 처리
-  메모리 주소는 다르더라도 email value의 값이 동일하다면 문제가 발생! 즉, 동일한 회원 email로 가입할 수 없게 해야 한다.
-  따라서 아래와 같은 == operator를 override해줘야 하는데 값이 같은 경우에도 같은 것으로 판단해야 하기 때문이다.
-  즉, 주소가 같고 값이 동일한 경우를 빨리 찾기(indentical)
-  주소는 다른데 값이 같은 경우를 찾기.. -> 아래에 구현
-   */
+  factory EmailAddress(String input) {
+    return EmailAddress._(validateEmailAddress(input));
+  }
+
+  const EmailAddress._(this.value);
+
+  /* 2.1 private underscore
+  1. static factory method만 해당 constructor에 접근해서 사용할 수 있도록 하거나,
+  2. object copying을 막기 위해서 사용하기도 한다.
+  3. 또는 해당 class가 unitliy 클레스인 경우에도 사용한다.
+  */
 
   @override
   bool operator ==(Object other) {
@@ -23,12 +32,34 @@ class EmailAddress {
     return other is EmailAddress && other.value == value;
   }
 
-  /* 1.3 hasCode
-  ==Operator를 override하게 되면 hashCode 또한 override해야 한다.
-  */
-
   @override
   int get hashCode => value.hashCode;
 }
 
-// 하지만 value에 우리가 원하는 email 값이 들어오지 않는 경우가 발생할 수 있다. 따라서 이를 수정할 필요가 있음
+/*
+2.3 validateEmailAddress
+적절한 input값인지를 아래의 함수에서 확인한다.
+만약 적절하지 않은 경우에는 Exeption의 기능들을 확장한 InvalidEmailException을 초기화하여 인스턴스르 반환하도록 한다.
+*/
+
+String validateEmailAddress(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return input;
+  } else {
+    throw InvalidEmailException(failedValue: input);
+  }
+}
+
+class InvalidEmailException implements Exception {
+  final String failedValue;
+
+  InvalidEmailException({required this.failedValue});
+}
+
+/*
+다음으로 넘어가기
+그런데 우리는 위와 같이 선언하고 사용하려고 한다면 try catch 문을 계속해서 사용해야 하는 번거로움이 발생하게 된다.
+이를 극복해보자.
+*/
